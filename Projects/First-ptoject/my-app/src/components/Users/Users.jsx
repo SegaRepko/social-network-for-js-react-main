a0adad1dd0f4c23a9e4b3488c858385cf40024ac
Sergey Repko <sergey.repko@vortexads.com>
2022-05-05 15:05:49 +0300

    add users API (get) 51

--- Projects/First-ptoject/my-app/src/components/Users/Users.jsx
+++ Projects/First-ptoject/my-app/src/components/Users/Users.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
 let Users = (props) => {
 
-
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, photoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true',
-                followed: false, fullName: 'Serhii', status: 'i developer', location: { city: 'Kyiv', contry: 'Ukraine' }
-            },
-            {
-                id: 2, photoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true',
-                followed: true, fullName: 'Liza', status: 'i design', location: { city: 'Lisabon', contry: 'Portugal' }
-            },
-            {
-                id: 3, photoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true',
-                followed: true, fullName: 'Sasha', status: 'i product manager', location: { city: 'Baku', contry: 'Azerbaijan' }
-            },
-            {
-                id: 4, photoUrl: 'https://filestore.community.support.microsoft.com/api/images/f2e55cbf-8316-4d3a-9412-ecd8194b2a72?upload=true',
-                followed: false, fullName: 'Kate', status: 'i scrum master', location: { city: 'Kyiv', contry: 'Ukraine' }
-            }
-        ]
-        )
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
+
     }
 
 
@@ -33,7 +21,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
@@ -43,12 +31,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
