4f6c4fd0fc1cc0ee2b6d3211762be8d24cf10777
Sergey Repko <sergey.repko@vortexads.com>
2022-05-09 16:43:24 +0300

    remove Side Effect 52

--- Projects/First-ptoject/my-app/src/components/Users/Users.jsx
+++ Projects/First-ptoject/my-app/src/components/Users/Users.jsx
@@ -5,18 +5,22 @@ import userPhoto from "../../assets/images/user.png";
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
+    let getUsers = () => {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items);
-            });
 
+        if (props.users.length === 0) {
+
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+
+        }
     }
 
 
-
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
