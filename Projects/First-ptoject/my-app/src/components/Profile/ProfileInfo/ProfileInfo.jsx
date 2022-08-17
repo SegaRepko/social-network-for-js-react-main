88d58ff824e0ea5d1cf69e75a181d6b7a24ac63c
Sergey Repko <sergey.repko@vortexads.com>
2022-08-17 20:36:30 +0300

    hook, useState, хуки 84

--- Projects/First-ptoject/my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ Projects/First-ptoject/my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import Preloader from '../../common/Preloader/Preloader';
 import classes from './ProfileInfo.module.css';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 
 const ProfileInfo = (props) => {
@@ -14,7 +15,7 @@ const ProfileInfo = (props) => {
             </div> */}
             <div className={classes.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-               <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+               <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     )
